7436d8ae7fb91a1d461788dc12a7f193fc16331b
Kenneth Chan <kicktheken@gmail.com>
2013-01-17 23:48:46 -0800

    generate repeating tabs

--- client/js/main.js
+++ client/js/main.js
@@ -3,25 +3,40 @@ define([
     "lib/bootstrap",
     "lib/spectrum"
 ],
-function() {
+function Main() {
     return function() {
         log.info("main start");
-        $('#tabsort').sortable({
+
+        var tabbar = $('#layer-tabbar').sortable({
             stop: function (e,ui) {
                 console.log([e,ui]);
             }
         });
+        var html = tabbar.html(), ohtml = html.replace(' class="active"','');
+        for (var i = 2; i <= 8; i++) {
+            var tab = ohtml;
+            if (i % 2 === 0) tab = tab.replace(/open/g,"close");
+            html += tab.replace(/(ayer( )?)1/g,"$1"+i);
+        }
+        tabbar.html(html);
+        
+        tabbar = $('#color-tabbar').sortable({
+            stop: function (e,ui) {
+                console.log([e,ui]);
+            }
+        });
+        html = tabbar.html(), ohtml = html.replace(' class="active"','');
+        for (var i = 2; i <= 10; i++) {
+            html += ohtml.replace(/color1/g,"color"+i);
+        }
+        tabbar.html(html);
+
         $('#signin').popover({
             placement: 'bottom',
             title: 'Sign in or Register',
             html: true,
             content: $('.signinform').html(),
             trigger: 'click'
-        });
-        $('.list-left').sortable({
-            stop: function (e,ui) {
-                console.log([e,ui]);
-            }
         });
         $('.colorpicker').spectrum({
             preferredFormat: 'name',
